cb6cb2d39585df082349bb2bda492b7657f2c717
Leonard Lorenz <dev@leonardlorenz>
2020-04-16 15:09:36 +0200

    per block solving implemented

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,36 @@ fn main() {
     let game = Sudoku::new();
     game.to_string();
 
-    let grid: [[u8; 9]; 9] =
-        [
-            [2, 0, 5, 0, 0, 9, 0, 0, 4],
-            [0, 0, 0, 0, 0, 0, 3, 0, 7],
-            [7, 0, 0, 8, 5, 6, 0, 1, 0],
-            [4, 5, 0, 7, 0, 0, 0, 0, 0],
-            [0, 0, 9, 0, 0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 0, 2, 0, 8, 5],
-            [0, 2, 0, 4, 1, 8, 0, 0, 6],
-            [6, 0, 8, 0, 0, 0, 0, 0, 0],
-            [1, 0, 0, 2, 0, 0, 7, 0, 8]
-        ];
+    let unfinished_grid: [[u8; 9]; 9] = [
+        [2, 0, 5, 0, 0, 9, 0, 0, 4],
+        [0, 0, 0, 0, 0, 0, 3, 0, 7],
+        [7, 0, 0, 8, 5, 6, 0, 1, 0],
+        [4, 5, 0, 7, 0, 0, 0, 0, 0],
+        [0, 0, 9, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 2, 0, 8, 5],
+        [0, 2, 0, 4, 1, 8, 0, 0, 6],
+        [6, 0, 8, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 2, 0, 0, 7, 0, 8],
+    ];
 
-    let easy_game = Sudoku::new_from_grid(grid);
-    easy_game.solve();
-    easy_game.to_string();
+    let finished_grid: [[u8; 9]; 9] = [
+        [4, 3, 5, 2, 6, 9, 7, 8, 1],
+        [6, 8, 2, 5, 7, 1, 4, 9, 3],
+        [1, 9, 7, 8, 3, 4, 5, 6, 2],
+        [8, 2, 6, 1, 9, 5, 3, 4, 7],
+        [3, 7, 4, 6, 8, 2, 9, 1, 5],
+        [9, 5, 1, 7, 4, 3, 6, 2, 8],
+        [5, 1, 9, 3, 2, 6, 8, 7, 4],
+        [2, 4, 8, 9, 5, 7, 1, 3, 6],
+        [7, 6, 3, 4, 1, 8, 2, 5, 9],
+    ];
+
+    let unfinished_game = Sudoku::new_from_grid(unfinished_grid);
+    let finished_game = Sudoku::new_from_grid(finished_grid);
+
+    Sudoku::is_solved(unfinished_game.clone());
+    unfinished_game.to_string();
+
+    Sudoku::is_solved(finished_game.clone());
+    finished_game.to_string();
 }
--- src/sudoku.rs
+++ src/sudoku.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use rand::prelude::*;
+use std::vec::Vec;
 
 /**
  * returns a 2 dimensional array filled with random numbers
@@ -12,34 +13,149 @@ fn generate_grid() -> [[u8; 9]; 9] {
 
     for x in grid.iter_mut() {
         for y in x.iter_mut() {
-            *y = rng.gen_range(0, 9);
+            *y = rng.gen_range(1, 9);
         }
     }
 
     grid
 }
 
+pub struct Block {
+    grid: [[u8; 3]; 3],
+}
+
+impl Block {
+    pub fn is_solved(&self) -> bool {
+        let mut grid: Vec<u8> = vec![0 as u8; 9];
+        let mut i = 0;
+        for x in self.grid.iter() {
+            for y in x.iter() {
+                grid[i] = *y;
+                i += 1;
+            }
+        }
+
+        // thanks floppy!
+        let rev_grid: Vec<u8> = grid.iter().copied().rev().collect();
+
+        for x in grid.iter() {
+            let mut i: u8 = 0;
+            for y in rev_grid.iter() {
+                if x == y {
+                    i += 1;
+                    if i == 2 {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    pub fn to_string(&self) {
+        for x in self.grid.iter() {
+            for y in x.iter() {
+                print!("{}", y.to_string());
+            }
+            print!("\n");
+        }
+    }
+}
+
 #[derive(Clone)]
 pub struct Sudoku {
-    grid: [[u8; 9]; 9]
+    grid: [[u8; 9]; 9],
 }
 
 impl Sudoku {
     pub fn new() -> Sudoku {
         Sudoku {
-            grid: generate_grid()
+            grid: generate_grid(),
         }
     }
 
     pub fn new_from_grid(new_grid: [[u8; 9]; 9]) -> Sudoku {
-        Sudoku {
-            grid: new_grid
-        }
+        Sudoku { grid: new_grid }
     }
 
-    pub fn solve(&self) -> Sudoku {
-        let solved = self.clone();
-        return solved;
+    ///**
+    // * Solves the Sudoku.
+    // */
+    //pub fn solve(self) {
+    //    let iterations: u64 = 0;
+    //    Sudoku::solve_rec(self, iterations);
+    //}
+
+    ///**
+    // * A recursive helper method for solve()
+    // */
+    //fn solve_rec(sudoku: mut Sudoku, mut iterations: u64) -> bool {
+    //    let mut current_number = 1;
+    //    for x in sudoku.grid.iter_mut() {
+    //        for y in x.iter_mut() {
+    //            if *y == 0 {
+    //                *y = current_number;
+    //            } else {
+    //                iterations += 1;
+    //                if Sudoku::is_solved(){
+    //                    current_number += 1;
+    //                    *y = current_number;
+    //                    Sudoku::solve_rec(sudoku.clone(), iterations);
+    //                } else {
+    //                    return true;
+    //                }
+    //            }
+    //        }
+    //    }
+    //}
+
+    /**
+     * takes three from column from index i and stores them in a Block
+     */
+    pub fn take_block(&self, x: usize, y: usize) -> Block {
+        let take_three_from_index =
+            |col: [u8; 9], i: usize| return [col[i], col[i + 1], col[i + 2]];
+
+        // takes block of 3x3 from coordinates
+        // x = width coord
+        // y = height coord
+        let new_block = [
+            take_three_from_index(self.grid[y], x),
+            take_three_from_index(self.grid[y + 1], x),
+            take_three_from_index(self.grid[y + 2], x),
+        ];
+        Block { grid: new_block }
+    }
+
+    /**
+     * Tests if a sudoku is solved or not.
+     */
+    pub fn is_solved(sudoku: Sudoku) -> bool {
+        // we need to part the game field in 9 boxes.
+        // those we test first
+        // every row, column, and box needs to have every single number from
+        // 1 - 9 in them without duplicates
+        let blocks: [Block; 9] = [
+            sudoku.take_block(0, 0),
+            sudoku.take_block(3, 0),
+            sudoku.take_block(6, 0),
+            sudoku.take_block(0, 3),
+            sudoku.take_block(3, 3),
+            sudoku.take_block(6, 3),
+            sudoku.take_block(0, 6),
+            sudoku.take_block(3, 6),
+            sudoku.take_block(6, 6),
+        ];
+
+        for block in blocks.iter() {
+            block.to_string();
+            if !block.is_solved() {
+                println!("Block not solved.");
+                return false;
+            }
+        }
+
+        return true;
     }
 
     /**
